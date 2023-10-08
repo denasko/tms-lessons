60b053e85fcae83f2eef8702063421942c88d7f4
Denas <145489632+denasko@users.noreply.github.com>
2023-10-08 19:28:21 +0300

    Update task_02_01.py

--- lesson_06/task_02_01.py
+++ lesson_06/task_02_01.py
@@ -3,7 +3,9 @@ from task_01 import input_list
 # Дан список чисел. Увеличьте каждый элемент в 100 раз.
 for i in input_list():
     print([i * 100], end=' ')
+
 print([i * 100 for i in input_list()])
+
 print(list(map(lambda x: x * 100, input_list())))
 
 # Дан список чисел. Преобразуйте этот список в список строк.
@@ -11,12 +13,16 @@ num = []
 for i in input_list():
     num.append(str(i))
 print(num)
+
 print([str(i) for i in input_list()])
+
 print(list(map(str, input_list())))
 
 # Дан список чисел.
 # Разделите каждый элемент на 100 и округлите до целого числа (функция round).
 for i in input_list():
     print(round(i / 100), end=' ')
+
 print([round(i / 100) for i in input_list()])
+
 print(list(map(lambda x: round(x / 100), input_list())))
